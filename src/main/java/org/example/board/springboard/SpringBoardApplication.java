066b2fc63b960e8f554c1fef37c00ede2bf638bd
jeomseon0516 <126733377+jeomseon0516@users.noreply.github.com>
2026-06-29 11:53:27 +0900

    commit spring-board

---
+++ src/main/java/org/example/board/springboard/SpringBoardApplication.java
@@ -0,0 +1,13 @@
+package org.example.board.springboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoardApplication.class, args);
+    }
+
+}
